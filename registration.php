89011e8c6c72e87e6c47b8fc32660da4887b560d
erigoo <ericka.calvo@pcc.edu>
2017-12-12 18:40:53 -0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -9,7 +9,6 @@ $emergency   = check_input($_POST['emergency']);
 $emergencyphone = check_input($_POST['emergency phone']);
 $event = check_input($_POST['event']);
 
-
 if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
 {
     show_error("E-mail address not valid");
